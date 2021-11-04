662554c4233c87aa3479be1b3c45f39cc663304b
Cleves <cleves0315@foxmail.com>
2021-11-05 01:58:36 +0800

    feat: edit title function

--- src/scripts/options.js
+++ src/scripts/options.js
@@ -8,7 +8,8 @@ const template = (data) => (`
     <div class="sign-item">
       <div class="del-wrap" data-id="${data.id}"><div class="del-btn" data-id="${data.id}"></div></div>
       <div class="icon"><img src="${data.favIconUrl}" /></div>
-      <div class="title"><p>${data.title}</p></div>
+      <div class="title" data-type="text"><p>${data.title}</p></div>
+      <div class="title" data-type="input" style="display: none;"><input data-id="${data.id}" value="${data.title}" /></div>
     </div>
   </a>
 `)
@@ -27,18 +28,44 @@ function getSign() {
 }
 
 function handleToDelSign(e) {
-  console.log('handleToDelSign: ', e.target.getAttribute('data-id'))
   e.preventDefault()
   const id = e.target.getAttribute('data-id')
 
   storage.get('sign', function(result) {
-    console.log('sign: ', result)
     if (result && result.sign) {
       const sign = (typeof result.sign === 'string') ? JSON.parse(result.sign) : result.sign
       const findIndex = sign.findIndex(m => m.id === id)
-      console.log('findindex: ', findIndex)
       sign.splice(findIndex, 1)
-      console.log('sign: ', sign)
+      storage.set({ sign: sign }, function() {
+        getSign()
+      })
+    }
+  })
+}
+
+function handleToEdit(e) {
+  e.preventDefault()
+  console.log(e)
+  this.parentNode.style['display'] = 'none'
+  this.parentNode.nextElementSibling.style['display'] = 'block'
+  this.parentNode.nextElementSibling.querySelector('input').focus()
+}
+
+function handleToEnterInput(e) {
+  if (e.keyCode === 13) {
+    handleToCancelEdit(e)
+  }
+}
+
+function handleToCancelEdit(e) {
+  const value = e.target.value
+  const id = e.target.getAttribute('data-id')
+
+  storage.get('sign', function(result) {
+    if (result && result.sign) {
+      const sign = (typeof result.sign === 'string') ? JSON.parse(result.sign) : result.sign
+      const findIndex = sign.findIndex(m => m.id === id)
+      sign[findIndex].title = value
       storage.set({ sign: sign }, function() {
         getSign()
       })
@@ -47,9 +74,13 @@ function handleToDelSign(e) {
 }
 
 function addElementEvents() {
-  document.querySelectorAll('.del-wrap')
+  document.querySelectorAll('.sign-item')
     .forEach(elm => {
-      elm.addEventListener('click', handleToDelSign)
+      elm.querySelector('.del-wrap').addEventListener('click', handleToDelSign)
+      elm.querySelector('.title[data-type="text"]').querySelector('p').addEventListener('click', handleToEdit)
+      elm.querySelector('.title[data-type="input"]').addEventListener('click', function(e) { e.preventDefault() })
+      elm.querySelector('.title[data-type="input"]').querySelector('input').addEventListener('blur', handleToCancelEdit)
+      elm.querySelector('.title[data-type="input"]').querySelector('input').addEventListener('keydown', handleToEnterInput)
     })
 }
 
