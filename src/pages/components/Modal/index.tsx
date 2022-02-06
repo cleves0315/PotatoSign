8f4d476a41bf996eabfce6554f19209326743f0d
cleves0315 <cleves0315@foxmail.com>
2022-02-06 20:54:27 +0800

    feat: perfect modal

--- src/pages/components/Modal/index.tsx
+++ src/pages/components/Modal/index.tsx
@@ -1,28 +1,79 @@
-import React, { ReactElement, useState } from 'react';
+import React, { useState } from 'react';
 import './index.scss';
 
-interface Props {}
+interface Props {
+  visible: boolean;
+  title?: string;
+  width?: string | number;
+  inputValue?: string;
+  submitText?: string;
+  maxLength?: number;
+  onSumbit?: (val: string) => void;
+}
 
-const DropdownMenu: React.FC<Props> = ({}: Props) => {
+const DropdownMenu: React.FC<Props> = ({
+  visible,
+  title,
+  width,
+  inputValue,
+  submitText,
+  maxLength,
+  onSumbit,
+}: Props) => {
   const ModalClass = 'modal';
-  const [left, setLeft] = useState(0);
+  const [inputVal, setInputValue] = useState('');
+
+  const onInputChange = (e: any) => {
+    const { value } = e.target;
+    setInputValue(value);
+  };
+
+  const handleOnSubmit = () => {
+    const value = inputVal.trim() || inputValue?.trim() || '';
+    onSumbit && onSumbit(value);
+  };
+
+  const isBtnDisabled = (): boolean => {
+    return !(inputValue?.trim() || inputVal.trim());
+  };
+
+  const modalStyle = {
+    width: typeof width === 'number' ? `${width}px` : width,
+  };
 
   return (
     <div>
       <div className={`${ModalClass}-root`}>
-        <div className={`${ModalClass}-mask`}></div>
-        <div className={`${ModalClass}-wrap`}>
-          <div className={`${ModalClass}-wrapper`}>
+        {visible && <div className={`${ModalClass}-mask`}></div>}
+        <div
+          className={`${ModalClass}-wrap`}
+          style={!visible ? { display: 'none' } : {}}
+        >
+          <div className={`${ModalClass}-wrapper`} style={modalStyle}>
             <div className={`${ModalClass}-content`}>
               <div className={`${ModalClass}-header`}>
-                <div className={`${ModalClass}-title`}>标题</div>
+                <div className={`${ModalClass}-title`}>{title || '标题'}</div>
               </div>
               <div className={`${ModalClass}-body`}>
-                <input className="input-borderless" type="text" value={123} />
+                <input
+                  className="input-borderless"
+                  type="text"
+                  value={inputValue || inputVal}
+                  maxLength={maxLength || 100}
+                  onChange={onInputChange}
+                />
               </div>
               <div className={`${ModalClass}-footer`}>
                 <div className="footer">
-                  <button className="button">确定</button>
+                  <button
+                    className={`button ${
+                      isBtnDisabled() ? 'button-disabled' : ''
+                    }`}
+                    disabled={isBtnDisabled()}
+                    onClick={handleOnSubmit}
+                  >
+                    {submitText || '确定'}
+                  </button>
                 </div>
               </div>
             </div>
