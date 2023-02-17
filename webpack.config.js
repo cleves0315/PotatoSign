ff7dbc37a72477704d25243965efb94660b791da
cleves0315 <cleves0315@foxmail.com>
2023-02-17 16:35:45 +0800

    chore: 增加图片处理 roader

--- webpack.config.js
+++ webpack.config.js
@@ -19,10 +19,10 @@ module.exports = {
   },
   module: {
     rules: [
-      // {
-      //   test: /\.(png|svg|jpg|jpeg|gif)$/i,
-      //   type: "asset/resource",
-      // },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
       {
         test: /\.html$/i,
         loader: "html-loader",
